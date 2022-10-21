3283e3eb2078b3461aa1c64eeb26cfd3b153c58e
JohnMichael_Erestain <erestainjm15@gmail.com>
2022-10-21 09:56:03 +0800

    added present.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const read = require('./read')
 const add = require('./add')
-// const present = require('./present')
+const present = require('./present')
 // const remove = require('./remove')
 // const update = require('./update') 
 
@@ -15,4 +15,10 @@ const add = require('./add')
 }
     var oldNote = read()
     add(note, oldNote)
+}
+
+
+if(data[2] == 'read') {
+
+present(read()) 
 }
